282d6ee1078c3f943bbed7a9d91636f1d94964c9
mimansamaheshwari <mimansamaheshwari@gmail.com>
2019-01-11 12:13:39 +0530

    Update recordVideos.ino

--- examples/Uno Nano Mega/07.Camera/recordVideos/recordVideos.ino
+++ examples/Uno Nano Mega/07.Camera/recordVideos/recordVideos.ino
@@ -35,7 +35,7 @@ void loop() {
   char a = processSerialdata();
   if( a == '1')
   {
-    Camera.setParameters(Front,Off,High,0);
+    Camera.setParameters(Front,Off,High,0);  //Direction , Flash, Quality, zoom(0-100%)
     Camera.captureImage();
     Camera.startRecording();
   }
